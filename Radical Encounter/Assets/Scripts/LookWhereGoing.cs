1ec0dd6d198f23366f1d1c35e167fba20e2f8439
sherzock <jacobo.galofre@gmail.com>
2019-11-02 12:39:59 +0100

    Look where going implemented

---
+++ Radical Encounter/Assets/Scripts/LookWhereGoing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookWhereGoing : MonoBehaviour
+{
+    public float RotSpeed = 5.5f;
+    MovementManager Movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Movement = GetComponent<MovementManager>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        steer();
+    }
+
+    public void steer()
+    {
+        Vector3 direction = Movement.movement;
+        float step = RotSpeed * Time.deltaTime;
+
+        Vector3 FinalOrientation = Vector3.RotateTowards(transform.forward, direction, step, 0.0f);
+        transform.rotation = Quaternion.LookRotation(FinalOrientation);
+    }
+
+}

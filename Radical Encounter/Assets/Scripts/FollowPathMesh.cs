e5f0fadf983cb07fff94eb1a9423e4e186a7c9a9
sherzock <jacobo.galofre@gmail.com>
2019-11-03 10:54:05 +0100

    FollowPathMesh implemented

---
+++ Radical Encounter/Assets/Scripts/FollowPathMesh.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPathMesh : MonoBehaviour
+{
+    MovementManager movement;
+    private NavMeshPath path;
+    Vector3 Subtarget;
+    Arrive arrive;
+    Seek seek;
+
+    void Start()
+    {
+        seek = GetComponent<Seek>();
+        arrive = GetComponent<Arrive>();
+        movement = GetComponent<MovementManager>();
+        path = new NavMeshPath();
+    }
+
+    void Update()
+    {
+        Steer(movement.target.transform.position);
+    }
+
+    public void Steer(Vector3 targ)
+    {
+        NavMesh.CalculatePath(transform.position, targ, NavMesh.AllAreas, path);
+        Subtarget = path.corners[1];
+        float distance = Subtarget.magnitude - transform.position.magnitude;
+
+        if(distance < 0.5f && path.corners.Length > 2)
+        { NavMesh.CalculatePath(transform.position, targ, NavMesh.AllAreas, path); }
+
+        if(path.corners.Length > 2) { seek.Steer(Subtarget); }
+        else { arrive.Steer(Subtarget); }
+    }
+}

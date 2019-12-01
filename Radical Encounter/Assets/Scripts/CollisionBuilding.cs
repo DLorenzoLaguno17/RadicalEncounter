5ecd77f7a5ede80b6edbe38c3b74d4e571800e1c
sherzock <jacobo.galofre@gmail.com>
2019-12-01 03:30:31 +0100

    Now Buildings Should Detect bullets and low the HP if they collide to the building

---
+++ Radical Encounter/Assets/Scripts/CollisionBuilding.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionBuilding : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "SoldierBullet")
+        {
+            if(transform.parent.GetComponent<DestroyableBuildingsBehaviour>().HP > 15)
+            {
+                transform.parent.GetComponent<DestroyableBuildingsBehaviour>().HP -= 15;
+            }
+            else if(transform.parent.GetComponent<DestroyableBuildingsBehaviour>().HP < 15)
+            {
+                transform.parent.GetComponent<DestroyableBuildingsBehaviour>().HP = 0;
+            }
+        }
+    }
+}
--- Radical Encounter/Assets/Scripts/DestroyableBuildingsBehaviour.cs
+++ Radical Encounter/Assets/Scripts/DestroyableBuildingsBehaviour.cs
@@ -5,9 +5,7 @@ using UnityEngine;
 public class DestroyableBuildingsBehaviour : MonoBehaviour
 {
     public int HP;
-    GameObject FULL;
-    GameObject HALF;
-    GameObject NONE;
+    GameObject FULL, HALF, NONE;
     Collider BBOX;
     MeshRenderer mesh;
 
@@ -26,26 +24,39 @@ public class DestroyableBuildingsBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(HP > 100)
+        UpdateHP();
+        RenderChoice();
+    }
+
+    public void UpdateHP()
+    {
+
+    }
+
+    public void RenderChoice()
+    {
+        if (HP > 100)
         {
             mesh.enabled = true;
             HALF.SetActive(false);
             NONE.SetActive(false);
 
         }
-        else if(HP > 50 && HP < 100)
+        else if (HP > 50 && HP < 100)
         {
             HALF.SetActive(true);
             NONE.SetActive(false);
             mesh.enabled = false;
         }
-        else if(HP < 50 && HP >= 0)
+        else if (HP < 50 && HP >= 0)
         {
             NONE.SetActive(true);
             HALF.SetActive(false);
             mesh.enabled = false;
         }
     }
+       
+        
 
     public GameObject FindCHILD(string str)
     {

9ffcae6dd75f15a042039dd4dea213080e60b0a7
sherzock <jacobo.galofre@gmail.com>
2019-12-12 00:52:11 +0100

    Now can Rotate camera with A and D

---
+++ Radical Encounter/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform views;
+    public float transitionSpeed;
+    Transform currentView;
+    bool FirstTimeCamera = true;
+
+    void Start()
+    {
+        StartCoroutine(StopMovement());
+    }
+
+    void Update()
+    {
+        if(FirstTimeCamera)
+        {
+            currentView = views;
+        }
+
+    }
+
+
+    void LateUpdate()
+    {
+        if(FirstTimeCamera)
+        {
+        //Lerp position
+        transform.position = Vector3.Lerp(transform.position, currentView.position, Time.deltaTime * transitionSpeed);
+
+        Vector3 currentAngle = new Vector3(
+         Mathf.LerpAngle(transform.rotation.eulerAngles.x, currentView.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+         Mathf.LerpAngle(transform.rotation.eulerAngles.y, currentView.transform.rotation.eulerAngles.y, Time.deltaTime * transitionSpeed),
+         Mathf.LerpAngle(transform.rotation.eulerAngles.z, currentView.transform.rotation.eulerAngles.z, Time.deltaTime * transitionSpeed));
+
+        transform.eulerAngles = currentAngle;
+
+        }
+
+    }
+
+    IEnumerator StopMovement()
+    {
+
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(5);
+
+        FirstTimeCamera = false;
+    }
+}
---
+++ Radical Encounter/Assets/Scripts/CameraRotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotator : MonoBehaviour
+{
+    public float speed = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            speed = 20.0f;
+
+        if (Input.GetKey(KeyCode.D))
+            speed = -20.0f;
+
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+
+        speed = 0.0f;
+    }
+}

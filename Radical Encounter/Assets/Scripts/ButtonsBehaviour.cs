0fb6a4328f49f94a4950e80403a87433d33998a8
sherzock <jacobo.galofre@gmail.com>
2019-12-11 20:30:13 +0100

    Main Menu Finished

--- Radical Encounter/Assets/Scripts/ButtonsBehaviour.cs
+++ Radical Encounter/Assets/Scripts/ButtonsBehaviour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ButtonsBehaviour : MonoBehaviour
 {
@@ -30,5 +31,57 @@ public class ButtonsBehaviour : MonoBehaviour
         {
             GoToRepair.GetComponentInParent<DestroyableBuildingsBehaviour>().HP = 150;
         }
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        GameObject Play = FindCHILD("Play");
+        GameObject Credits = FindCHILD("Credits");
+        GameObject Quit = FindCHILD("Quit");
+        GameObject Back = FindCHILD("Back");
+        GameObject CreditsText = FindCHILD("Credits Text");
+
+        Play.SetActive(false);
+        Credits.SetActive(false);
+        Quit.SetActive(false);
+        Back.SetActive(true);
+        CreditsText.SetActive(true);
+    }
+
+    public void Back()
+    {
+        GameObject Play = FindCHILD("Play");
+        GameObject Credits = FindCHILD("Credits");
+        GameObject Quit = FindCHILD("Quit");
+        GameObject Back = FindCHILD("Back");
+        GameObject CreditsText = FindCHILD("Credits Text");
+
+        Play.SetActive(true);
+        Credits.SetActive(true);
+        Quit.SetActive(true);
+        Back.SetActive(false);
+        CreditsText.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public GameObject FindCHILD(string str)
+    {
+        foreach (Transform eachChild in transform)
+        {
+            if (eachChild.name == str)
+            {
+                return eachChild.gameObject;
+            }
+        }
+        return null;
     }
 }
